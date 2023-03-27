b5df9d8f338993aff2970eb76b434fdbcd139e01
Taqaddus <taqaddusshafi9086@gmail.com>
2023-03-27 22:14:55 +0530

    new

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -3,7 +3,7 @@ import { NavLink as Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Nav = styled.nav`
-background: #63D471;
+background: #b8d119;
 height: 85px;
 display: flex;
 justify-content: space-between;
@@ -14,7 +14,7 @@ z-index: 12;
 `;
 
 export const NavLink = styled(Link)`
-color: #808080;
+color: #f20505;
 display: flex;
 align-items: center;
 text-decoration: none;
@@ -81,6 +81,6 @@ margin-left: 24px;
 &:hover {
 	transition: all 0.2s ease-in-out;
 	background: #fff;
-	color: #808080;
+	color: #908580;
 }
 `;
